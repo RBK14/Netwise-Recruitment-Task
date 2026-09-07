b975337274fe4473f2373c2e57c7979f0cb1a6f3
Maciej Rybka <maciek.rybka2004@gmail.com>
2026-09-07 15:47:52 +0200

    create cat fact client abstraction and implementation

---
+++ src/Netwise.RecruitmentTask/Services/Abstractions/ICatFactClient.cs
@@ -0,0 +1,8 @@
+﻿using Netwise.RecruitmentTask.Models.Domain;
+
+namespace Netwise.RecruitmentTask.Services.Abstractions;
+
+public interface ICatFactClient
+{
+    Task<CatFact?> GetCatFactAsync(CancellationToken cancellationToken = default);
+}
