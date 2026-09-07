7836fcbf9651427152210c2366de405b25b551cc
Maciej Rybka <maciek.rybka2004@gmail.com>
2026-09-07 16:32:30 +0200

    add cat fact processing handler abstraction and implemtation

---
+++ src/Netwise.RecruitmentTask/Execution/Abstractions/IProcessCatFactHandler.cs
@@ -0,0 +1,6 @@
+﻿namespace Netwise.RecruitmentTask.Execution.Abstractions;
+
+public interface IProcessCatFactHandler
+{
+    Task HandleAsync(CancellationToken cancellationToken = default);
+}
---
+++ src/Netwise.RecruitmentTask/Execution/ProcessCatFactHandler.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using Netwise.RecruitmentTask.Execution.Abstractions;
+using Netwise.RecruitmentTask.Services.Abstractions;
+
+namespace Netwise.RecruitmentTask.Execution;
+
+public class ProcessCatFactHandler(
+    ICatFactClient catFactClient,
+    IFileWriter fileWriter,
+    ILogger<ProcessCatFactHandler> logger) : IProcessCatFactHandler
+{
+    public async Task HandleAsync(CancellationToken cancellationToken = default)
+    {
+        var fact = await catFactClient.GetCatFactAsync(cancellationToken);
+
+        if (fact == null)
+        {
+            logger.LogWarning("No cat fact has been retrieved. Processing aborted.");
+            return;
+        }
+
+        await fileWriter.WriteFactAsync(fact, cancellationToken);
+    }
+}
